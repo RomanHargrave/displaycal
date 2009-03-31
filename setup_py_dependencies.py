5254653754a8df2c3e828b92c37eb0ece2c64984
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2009-03-31 22:04:42 +0000

    Install custom pure python modules used by dispcalGUI.py
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@25 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

---
+++ setup_py_dependencies.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+name = 'dispcalGUI_py_dependencies'
+version = '0.2.2b'
+setup(name=name,
+	  version=version,
+	  py_modules=['argyllRGB2XYZ', 
+				  'CGATS', 
+				  'colormath', 
+				  'ICCProfile', 
+				  'natsort', 
+				  'pyi_md5pickuphelper', 
+				  'safe_print', 
+				  'tempfile26', 
+				  'trash'
+				 ],
+	  author='Florian Höch',
+	  author_email='dispcalGUI@hoech.net',
+	  url='http://dispcalGUI.hoech.net/',
+	  download_url='http://dispcalGUI.hoech.net/dispcalGUI-%(version)-src.zip' % {'version': version},
+	  description='Python modules needed by dispcalGUI.py',
+	  long_description="""Python modules needed by dispcalGUI.py, a graphical
+	   user interface for the Argyll CMS display calibration utilities""",
+	  license='GNU GPL 3.0',
+	  keywords=['dispcalGUI'],
+	  platforms=[],
+	  classifiers=['Development Status :: 4 - Beta',
+				   'Intended Audience :: End Users/Advanced End Users',
+				   'License :: OSI Approved :: GNU General Public License (GPL)',
+				   'Operating System :: OS Independent',
+				   'Programming Language :: Python',
+				   'Topic :: Graphics'
+				  ]
+	  )
