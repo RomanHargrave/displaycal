0e77f913d4016fe40afdf8eb0905cd926e89e1f8
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2009-05-07 02:55:54 +0000

    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@142 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- util/dist.sh
+++ util/dist.sh
@@ -6,14 +6,4 @@
 ./setup.py bdist_pyi -F --use-distutils > pyi.log 2>&1
 
 # Python 2.5 RPM
-/usr/bin/python2.5 setup.py bdist_rpm --binary-only --use-distutils > rpm-py2.5.log 2>&1
-mkdir -p dist/python2.5
-mv dist/dispcalGUI-*.rpm dist/python2.5
-
-# Python 2.6 RPM
-/usr/local/bin/python2.6 setup.py bdist_rpm --binary-only --use-distutils > rpm-py2.6.log 2>&1
-mkdir -p dist/python2.6
-mv dist/dispcalGUI-*.rpm dist/python2.6
-
-# Source RPM and tarball
-./setup.py bdist_rpm --source-only --use-setuptools > rpm-src.log 2>&1
+/usr/bin/python2.5 setup.py bdist_rpm --use-distutils > rpm-py2.5.log 2>&1
