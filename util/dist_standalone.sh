c6b70aafa34e90f79e9fdde935c84271ea0d07da
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2009-06-05 22:17:21 +0000

    Fixed python version used for creating standalone binary
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@181 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- util/dist_standalone.sh
+++ util/dist_standalone.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-platform=`python -c "from distutils.util import get_platform;print get_platform()"`
-python_version=`python -c "import sys;print sys.version[:3]"`
-version=`python -c "from dispcalGUI import meta;print meta.version"`
+python=python2.6
+
+platform=`$python -c "from distutils.util import get_platform;print get_platform()"`
+python_version=`$python -c "import sys;print sys.version[:3]"`
+version=`$python -c "from dispcalGUI import meta;print meta.version"`
 
 # Standalone executable
 log=dispcalGUI-$version.pyi.$platform-py$python_version-onefile.log
-python2.6 setup.py bdist_pyi -F --use-distutils 2>&1 | tee $log
+$python setup.py bdist_pyi -F --use-distutils 2>&1 | tee $log
 
 # ZIP
 cd dist/pyi.$platform-py$python_version-onefile
