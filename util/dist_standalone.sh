00392a196fb23b7a2b92e35633890aa7d3a14667
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2009-06-05 22:09:03 +0000

    Fix directory name and always use Python 2.6
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@180 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- util/dist_standalone.sh
+++ util/dist_standalone.sh
@@ -5,9 +5,9 @@ python_version=`python -c "import sys;print sys.version[:3]"`
 version=`python -c "from dispcalGUI import meta;print meta.version"`
 
 # Standalone executable
-log=dispcalGUI-$version.pyi.$platform-$python_version-onefile.log
-./setup.py bdist_pyi -F --use-distutils 2>&1 | tee $log
+log=dispcalGUI-$version.pyi.$platform-py$python_version-onefile.log
+python2.6 setup.py bdist_pyi -F --use-distutils 2>&1 | tee $log
 
 # ZIP
-cd dist/pyi.$platform-$python_version-onefile
+cd dist/pyi.$platform-py$python_version-onefile
 tar -pczf dispcalGUI-$version-$platform.tar.gz dispcalGUI-$version 2>&1 | tee -a $log
