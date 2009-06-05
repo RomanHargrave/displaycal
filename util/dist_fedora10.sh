fd053b52af7a4b7561aab2feeb265e57e99ed331
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2009-06-05 21:18:21 +0000

    Use uname -m instead of -i to determine hardware platform in dist_*.sh scripts
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@178 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- util/dist_fedora10.sh
+++ util/dist_fedora10.sh
@@ -2,17 +2,17 @@
 
 dist=fedora10
 
-platform=`uname -i`
+platform=`uname -m`
 version=`python -c "from dispcalGUI import meta;print meta.version"`
 
 # Python 2.5 RPM for Fedora Argyll 1.0.3 package
 log=dispcalGUI-$version-py2.5-$dist-argyll.$platform.bdist_rpm.log
-/usr/bin/python2.5 setup.py bdist_rpm --cfg=$dist-argyll --skip-instrument-configuration-files --use-distutils 2>&1 | tee $log
+/usr/bin/python2.5 setup.py bdist_rpm --cfg=$dist-argyll --force-arch=$platform --skip-instrument-configuration-files --use-distutils 2>&1 | tee $log
 mv -f dist/dispcalGUI-$version-1.$platform.rpm dist/dispcalGUI-$version-py2.5-$dist-argyll-1.$platform.rpm 2>&1 | tee -a $log
 mv -f dist/dispcalGUI-$version-1.src.rpm dist/dispcalGUI-$version-py2.5-$dist-argyll-1.src.rpm 2>&1 | tee -a $log
 
 # Python 2.5 RPM for standalone Argyll executables
 log=dispcalGUI-$version-py2.5-$dist.$platform.bdist_rpm.log
-/usr/bin/python2.5 setup.py bdist_rpm --cfg=$dist --use-distutils 2>&1 | tee $log
+/usr/bin/python2.5 setup.py bdist_rpm --cfg=$dist --force-arch=$platform --use-distutils 2>&1 | tee $log
 mv -f dist/dispcalGUI-$version-1.$platform.rpm dist/dispcalGUI-$version-py2.5-$dist-1.$platform.rpm 2>&1 | tee -a $log
 mv -f dist/dispcalGUI-$version-1.src.rpm dist/dispcalGUI-$version-py2.5-$dist-1.src.rpm 2>&1 | tee -a $log
--- util/dist_ubuntu9.sh
+++ util/dist_ubuntu9.sh
@@ -2,12 +2,12 @@
 
 dist=ubuntu9
 
-platform=`uname -i`
+platform=`uname -m`
 version=`python -c "from dispcalGUI import meta;print meta.version"`
 
 # Python 2.6 DEB
 log=dispcalGUI_$version-py2.6-$dist.$platform.bdist_deb.log
-/usr/bin/python2.6 setup.py bdist_deb --cfg=$dist --use-distutils 2>&1 | tee $log
+/usr/bin/python2.6 setup.py bdist_deb --cfg=$dist --force-arch=$platform --use-distutils 2>&1 | tee $log
 mv -f dist/dispcalgui_$version-1_$platform.deb dist/dispcalgui_$version-py2.6-$dist-1_$platform.deb 2>&1 | tee -a $log
 mv -f dist/dispcalGUI-$version-1.$platform.rpm dist/dispcalGUI-$version-py2.6-$dist-1.$platform.rpm 2>&1 | tee -a $log
 mv -f dist/dispcalGUI-$version-1.src.rpm dist/dispcalGUI-$version-py2.6-$dist-1.src.rpm 2>&1 | tee -a $log
