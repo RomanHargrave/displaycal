b4006b5ceb4f0a0b9429e608e1600e2f9bbce827
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2010-01-16 19:09:22 +0000

    Added missing defaultpaths.py
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@288 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

---
+++ dispcalGUI/defaultpaths.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+if sys.platform == "win32":
+	try:
+		from win32com.shell.shell import SHGetSpecialFolderPath
+		from win32com.shell.shellcon import (CSIDL_APPDATA, 
+											 CSIDL_COMMON_APPDATA, 
+											 CSIDL_COMMON_STARTUP, 
+											 CSIDL_PROGRAM_FILES_COMMON, 
+											 CSIDL_STARTUP, CSIDL_SYSTEM)
+	except ImportError:
+		(CSIDL_APPDATA, CSIDL_COMMON_APPDATA, CSIDL_COMMON_STARTUP, 
+		 CSIDL_PROGRAM_FILES_COMMON, CSIDL_STARTUP, CSIDL_SYSTEM) = (26, 35, 
+																	 24, 43, 
+																	 7, 37)
+		def SHGetSpecialFolderPath(hwndOwner, nFolder):
+			return {
+				CSIDL_APPDATA: getenvu("APPDATA"),
+				CSIDL_COMMON_APPDATA: None,
+				CSIDL_COMMON_STARTUP: None,
+				CSIDL_PROGRAM_FILES_COMMON: getenvu("CommonProgramFiles"),
+				CSIDL_STARTUP: None,
+				CSIDL_SYSTEM: getenvu("SystemRoot")
+			}.get(nFolder)
+
+from util_os import expanduseru, getenvu
+
+if sys.platform == "win32":
+	appdata = SHGetSpecialFolderPath(0, CSIDL_APPDATA)
+	commonappdata = SHGetSpecialFolderPath(0, CSIDL_COMMON_APPDATA)
+	commonprogramfiles = SHGetSpecialFolderPath(0, CSIDL_PROGRAM_FILES_COMMON)
+	try:
+		autostart = SHGetSpecialFolderPath(0, CSIDL_COMMON_STARTUP)
+		# Can fail under Vista and later if directory doesn't exist
+	except Exception, exception:
+		autostart = None
+	try:
+		autostart_home = SHGetSpecialFolderPath(0, CSIDL_STARTUP)
+		# Can fail under Vista and later if directory doesn't exist
+	except Exception, exception:
+		autostart_home = None
+	iccprofiles = [os.path.join(SHGetSpecialFolderPath(0, CSIDL_SYSTEM), 
+								"spool", "drivers", "color")]
+	iccprofiles_home = iccprofiles
+elif sys.platform == "darwin":
+	prefs = os.path.join(os.path.sep, "Library", "Preferences")
+	prefs_home = os.path.join(expanduseru("~"), "Library", "Preferences")
+	appsupport = os.path.join(os.path.sep, "Library", "Application Support")
+	appsupport_home = os.path.join(expanduseru("~"), "Library", 
+								   "Application Support")
+	autostart = autostart_home = None
+	iccprofiles = [os.path.join(os.path.sep, "Library", "ColorSync", 
+								"Profiles"),
+				   os.path.join(os.path.sep, "System", "Library", "ColorSync", 
+								"Profiles")]
+	iccprofiles_home = [os.path.join(expanduseru("~"), "Library", "ColorSync", 
+									 "Profiles")]
+else:
+	xdg_config_home = getenvu("XDG_CONFIG_HOME",
+								  os.path.join(expanduseru("~"), ".config"))
+	xdg_config_dirs = getenvu("XDG_CONFIG_DIRS", 
+							  "/etc/xdg").split(os.pathsep)
+	xdg_data_home_default = expandvarsu("$HOME/.local/share")
+	xdg_data_home = getenvu("XDG_DATA_HOME", xdg_data_home_default)
+	xdg_data_dirs_default = "/usr/local/share:/usr/share"
+	xdg_data_dirs = getenvu("XDG_DATA_DIRS", 
+							xdg_data_dirs_default).split(os.pathsep)
+	for dir_ in xdg_data_dirs_default.split(os.pathsep):
+		if not dir_ in xdg_data_dirs:
+			xdg_data_dirs += [dir_]
+	del dir_
+	autostart = os.path.join(xdg_config_dirs[0], "autostart")
+	autostart_home = os.path.join(xdg_config_home, "autostart")
+	iccprofiles = [os.path.join(xdg_data_dirs[0], "color", "icc")]
+	iccprofiles_home = [os.path.join(xdg_data_home, "color", "icc")]
+	iccprofiles_display = os.path.join(iccprofiles, "devices", "display")
+	iccprofiles_display_home = os.path.join(iccprofiles_home, "devices", 
+											"display")
