1546a377c6c1d1778f43ea61042b88bfb3d8098c
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2010-02-26 15:58:14 +0000

    Removed pyinstaller as we've settled using pyapp on Mac OS X and py2exe on Windows, and the system python on Linux.
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@321 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- pyinstaller/buildtests/test-nestedlaunch1.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-import subprocess
-import datetime
-
-if __name__ == "__main__":
-    subprocess.check_call([os.path.dirname(sys.executable) + "/../test-nestedlaunch0/test-nestedlaunch0.exe"])
--- pyinstaller/hooks/hook-matplotlib.numerix.py
+++
@@ -1,6 +0,0 @@
-# Contributed by Peter Burgers
-# The matplotlib.numerix package sneaks these imports in under the radar:
-hiddenimports = ['fft',
-                 'linear_algebra',
-                 'random_array',
-                ]
