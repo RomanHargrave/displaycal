3ee587a4c0dbac2eb103b96036d4197351982fb4
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2010-06-03 21:54:46 +0000

    Updated presets. Default high profile quality, and calibration/profiling settings stored in embedded TI3.
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@410 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- util/createpresets.cmd
+++ util/createpresets.cmd
@@ -2,15 +2,15 @@
 setlocal
 
 for %%a in ("%~dp0..\misc\ti3\*.ti3") do (
-	if /i "%%~na"=="laptop"     call :createpreset "-qm -t -g2.2 -f1 -k0 colprof -qm -aS"           "Laptop"       "%%~dpna"
-	if /i "%%~na"=="office_web" call :createpreset "-qm -t6500 -g2.2 -f1 -k0 colprof -qm -aS"       "Office & Web" "%%~dpna"
-	if /i "%%~na"=="prepress"   call :createpreset "-qh -t5000 -b130 -g2.2 -f1 -k0 colprof -qh -ax" "Prepress"     "%%~dpna"
-	if /i "%%~na"=="photo"      call :createpreset "-qh -t5000 -g2.2 -f1 -k0 colprof -qh -ax"       "Photo"        "%%~dpna"
-	if /i "%%~na"=="video"      call :createpreset "-qh -t6500 -g2.2 -f1 -k0 colprof -qm -aS"       "Video"        "%%~dpna"
+	if /i "%%~na"=="laptop"     call :createpreset "Laptop"       "%%~dpna"
+	if /i "%%~na"=="office_web" call :createpreset "Office & Web" "%%~dpna"
+	if /i "%%~na"=="prepress"   call :createpreset "Prepress"     "%%~dpna"
+	if /i "%%~na"=="photo"      call :createpreset "Photo"        "%%~dpna"
+	if /i "%%~na"=="video"      call :createpreset "Video"        "%%~dpna"
 )
 goto :EOF
 
 :createpreset
-colprof  -v -ql -aG -C "Created with dispcalGUI and Argyll CMS: dispcal %~1" -D "dispcalGUI calibration preset: %~2" "%~3"
-move /-Y "%~dpn3.icm" "%~dp0..\dispcalGUI\presets\%~n3.icc"
+colprof  -v -ql -aG -C "Created with dispcalGUI and Argyll CMS" -D "dispcalGUI calibration preset: %~1" "%~2"
+move /-Y "%~dpn2.icm" "%~dp0..\dispcalGUI\presets\%~n2.icc"
 goto :EOF
