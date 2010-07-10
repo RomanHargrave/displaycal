8b4d5c3c876df184534cd703d14964e214c386ff
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2010-07-10 08:05:43 +0000

    Updated Windows convenience buildscript.
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@517 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- util/dist.cmd
+++ util/dist.cmd
@@ -8,16 +8,50 @@ for /F usebackq %%a in (`python -c "from dispcalGUI import meta;print meta.versi
 	set version=%%a
 )
 
+REM Source tarball
+if not exist dist\dispcalGUI-%version%.tar.gz (
+	python setup.py sdist --format=gztar --use-setuptools 2>&1 | tee dispcalGUI-%version%.sdist.log
+)
+
 REM Standalone executable
-setup.py bdist_standalone inno --use-distutils
+if not exist dist\py2exe.win32-py%python_version%\dispcalGUI-%version% (
+	python setup.py bdist_standalone inno --use-distutils 2>&1 | tee dispcalGUI-%version%.bdist_standalone-py%python_version%.log
+)
 
-REM ZIP
-pushd dist\py2exe.win32-py%python_version%
-zip -9 -r ..\dispcalGUI-%version%-win32.zip dispcalGUI-%version%
-popd
+REM Standalone executable - Setup
+if not exist dist\dispcalGUI-%version%-Setup.exe (
+	"C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc dist/dispcalGUI-Setup-py2exe.win32-py%python_version%.iss
+)
 
-REM Python 2.5 Installer
-C:\Python25\python.exe setup.py bdist_wininst --use-distutils 2>&1 | tee wininst-py2.5.log
+REM Standalone executable - ZIP
+if not exist dist\dispcalGUI-%version%-win32.zip (
+	pushd dist\py2exe.win32-py%python_version%
+	zip -9 -r ..\dispcalGUI-%version%-win32.zip dispcalGUI-%version%
+	popd
+)
 
-REM Python 2.6 Installer
-C:\Python26\python.exe setup.py bdist_wininst --use-distutils 2>&1 | tee wininst-py2.6.log
+if "%~1"=="bdist_msi" (
+	REM Python 2.5 MSI
+	if not exist dist\dispcalGUI-%version%.win32-py2.5.exe (
+		REM C:\Python25\python.exe setup.py bdist_msi --use-distutils 2>&1 | tee dispcalGUI-%version%.wininst-py2.5.log
+		REM C:\Python25\python.exe setup.py finalize_msi --use-distutils 2>&1 | tee -a dispcalGUI-%version%.wininst-py2.5.log
+	)
+
+	REM Python 2.6 MSI
+	if not exist dist\dispcalGUI-%version%.win32-py2.6.exe (
+		C:\Python26\python.exe setup.py bdist_msi --use-distutils 2>&1 | tee dispcalGUI-%version%.wininst-py2.6.log
+		C:\Python25\python.exe setup.py finalize_msi --use-distutils 2>&1 | tee -a dispcalGUI-%version%.wininst-py2.6.log
+	)
+)
+
+if "%~1"=="bdist_wininst" (
+	REM Python 2.5 Installer
+	if not exist dist\dispcalGUI-%version%.win32-py2.5.exe (
+		REM C:\Python25\python.exe setup.py bdist_wininst --use-distutils 2>&1 | tee dispcalGUI-%version%.wininst-py2.5.log
+	)
+
+	REM Python 2.6 Installer
+	if not exist dist\dispcalGUI-%version%.win32-py2.6.exe (
+		C:\Python26\python.exe setup.py bdist_wininst --user-access-control=auto --use-distutils 2>&1 | tee dispcalGUI-%version%.wininst-py2.6.log
+	)
+)
