da16c986a91d0c87abb0d6821abb71a88c4d54d9
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2010-07-25 06:30:35 +0000

    Updated check-install utility script
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@569 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- util/dispcalGUI-check-install.sh
+++ util/dispcalGUI-check-install.sh
@@ -1,56 +1,38 @@
 #!/bin/bash
 
+echo -e "\033[37;40m*** Checking $HOME/.config\033[0m"
+
+ls -l -G -h --color=always --group-directories-first -R $HOME/.config/autostart/*dispcalGUI* 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R $HOME/.config/dispcalGUI 2>/dev/null && echo
+
+echo "--------------------------------------------------------------------------------"
+
 echo -e "\033[37;40m*** Checking $HOME/.local\033[0m"
 
-ls -l -G -h --color=always $HOME/.local/bin/dispcalGUI 2>/dev/null && echo
+ls -l -G -h --color=always $HOME/.local/bin/dispcalGUI* 2>/dev/null && echo
 
 ls -l -G -h --color=always --group-directories-first -R $HOME/.local/lib/python*/*-packages/dispcalGUI* 2>/dev/null && echo
 
 ls -l -G -h --color=always $HOME/.local/share/applications/dispcalGUI.desktop 2>/dev/null && echo
 
-ls -l -G -h --color=always --group-directories-first -R $HOME/.local/share/dispcalGUI 2>/dev/null && echo
+ls -l -G -h --color=always --group-directories-first -R $HOME/.local/share/man/man1/dispcalGUI* 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R $HOME/.local/share/pyshared/dispcalGUI 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R $HOME/.local/share/pyshared-data/dispcalgui 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first $HOME/.local/share/dispcalGUI 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R $HOME/.local/share/doc/dispcalgui 2>/dev/null && echo
 
 ls -l -G -h --color=always --group-directories-first -R $HOME/.local/share/doc/dispcalGUI* 2>/dev/null && echo
 
 ls -l -G -h --color=always --group-directories-first -R $HOME/.local/share/doc/packages/dispcalGUI* 2>/dev/null && echo
 
+ls -l -G -h --color=always --group-directories-first -R $HOME/.local/share/doc-base/dispcalgui* 2>/dev/null && echo
+
 ls -l -G -h --color=always $HOME/.local/share/icons/hicolor/*/apps/dispcalGUI.png 2>/dev/null
-
-echo "--------------------------------------------------------------------------------"
-
-echo -e "\033[37;40m*** Checking /usr/local\033[0m"
-
-ls -l -G -h --color=always /usr/local/bin/dispcalGUI 2>/dev/null && echo
-
-ls -l -G -h --color=always --group-directories-first -R /usr/local/lib/python*/*-packages/dispcalGUI* 2>/dev/null && echo
-
-ls -l -G -h --color=always /usr/local/share/applications/dispcalGUI.desktop 2>/dev/null && echo
-
-ls -l -G -h --color=always --group-directories-first -R /usr/local/share/dispcalGUI 2>/dev/null && echo
-
-ls -l -G -h --color=always --group-directories-first -R /usr/local/share/doc/dispcalGUI* 2>/dev/null && echo
-
-ls -l -G -h --color=always --group-directories-first -R /usr/local/share/doc/packages/dispcalGUI* 2>/dev/null && echo
-
-ls -l -G -h --color=always /usr/local/share/icons/hicolor/*/apps/dispcalGUI.png 2>/dev/null
-
-echo "--------------------------------------------------------------------------------"
-
-echo -e "\033[37;40m*** Checking /usr\033[0m"
-
-ls -l -G -h --color=always /usr/bin/dispcalGUI 2>/dev/null && echo
-
-ls -l -G -h --color=always --group-directories-first -R /usr/lib/python*/*-packages/dispcalGUI* 2>/dev/null && echo
-
-ls -l -G -h --color=always /usr/share/applications/dispcalGUI.desktop 2>/dev/null && echo
-
-ls -l -G -h --color=always --group-directories-first -R /usr/share/dispcalGUI 2>/dev/null && echo
-
-ls -l -G -h --color=always --group-directories-first -R /usr/share/doc/dispcalGUI* 2>/dev/null && echo
-
-ls -l -G -h --color=always --group-directories-first -R /usr/share/doc/packages/dispcalGUI* 2>/dev/null && echo
-
-ls -l -G -h --color=always /usr/share/icons/hicolor/*/apps/dispcalGUI.png 2>/dev/null
 
 echo "--------------------------------------------------------------------------------"
 
@@ -66,15 +48,71 @@ ls -l -G -h --color=always --group-directories-first -R /etc/udev/*/*-Argyll.* 2
 
 echo "--------------------------------------------------------------------------------"
 
-echo -e "\033[37;40m*** Checking /usr/share/PolicyKit/policy\033[0m"
+echo -e "\033[37;40m*** Checking /etc/xdg/autostart\033[0m"
 
-ls -l -G -h --color=always /usr/share/PolicyKit/policy/color-device-file.policy 2>/dev/null
+ls -l -G -h --color=always --group-directories-first -R /etc/xdg/autostart/*dispcalGUI* 2>/dev/null
 
 echo "--------------------------------------------------------------------------------"
 
-echo -e "\033[37;40m*** Checking /usr/share/hal/fdi/policy/10osvendor\033[0m"
+echo -e "\033[37;40m*** Checking /usr/local\033[0m"
 
-ls -l -G -h --color=always /usr/share/hal/fdi/policy/10osvendor/19-color.fdi 2>/dev/null
+ls -l -G -h --color=always /usr/local/bin/dispcalGUI* 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/local/lib/python*/*-packages/dispcalGUI* 2>/dev/null && echo
+
+ls -l -G -h --color=always /usr/local/share/applications/dispcalGUI.desktop 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/local/share/man/man1/dispcalGUI* 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/local/share/pyshared/dispcalGUI 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/local/share/pyshared-data/dispcalgui 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/local/share/dispcalGUI 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/local/share/doc/dispcalgui 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/local/share/doc/dispcalGUI* 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/local/share/doc/packages/dispcalGUI* 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/local/share/doc-base/dispcalgui* 2>/dev/null && echo
+
+ls -l -G -h --color=always /usr/local/share/icons/hicolor/*/apps/dispcalGUI.png 2>/dev/null
+
+echo "--------------------------------------------------------------------------------"
+
+echo -e "\033[37;40m*** Checking /usr\033[0m"
+
+ls -l -G -h --color=always /usr/bin/dispcalGUI* 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/lib/python*/*-packages/dispcalGUI* 2>/dev/null && echo
+
+ls -l -G -h --color=always /usr/share/applications/dispcalGUI.desktop 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/share/man/man1/dispcalGUI* 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/share/pyshared/dispcalGUI 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/share/pyshared-data/dispcalgui 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/share/dispcalGUI 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/share/doc/dispcalgui 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/share/doc/dispcalGUI* 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/share/doc/packages/dispcalGUI* 2>/dev/null && echo
+
+ls -l -G -h --color=always --group-directories-first -R /usr/share/doc-base/dispcalgui* 2>/dev/null && echo
+
+ls -l -G -h --color=always /usr/share/icons/hicolor/*/apps/dispcalGUI.png 2>/dev/null
+
+echo "--------------------------------------------------------------------------------"
+
+echo -e "\033[37;40m*** Checking /var/lib/doc-base\033[0m"
+
+ls -l -G -h --color=always --group-directories-first -R /var/lib/doc-base/documents/dispcalgui* 2>/dev/null && echo
 
 echo "--------------------------------------------------------------------------------"
 
