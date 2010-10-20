f6c3bd25a80a2074329a2556037380e4cc48cd65
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2010-10-20 00:04:47 +0000

    Use constants instead of magic numbers for offsets. Check for valid gamma when parsing EDID. Parse red, green, blue and whitepoint chromaticities. Fixed parsing of color point data (will no longer override existing chromaticities and gamma unless they are zero). Determine number of extension blocks and validate checksum.
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@648 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- dispcalGUI/edid.py
+++ dispcalGUI/edid.py
@@ -3,6 +3,7 @@
 
 from hashlib import md5
 import codecs
+import math
 import os
 import struct
 import sys
@@ -23,9 +24,44 @@ elif sys.platform != "darwin":
 	except ImportError:
 		pass
 
+HEADER = (0, 8)
+MANUFACTURER_ID = (8, 10)
+PRODUCT_ID = (10, 12)
+SERIAL_32 = (12, 16)
+WEEK_OF_MANUFACTURE = 16
+YEAR_OF_MANUFACTURE = 17
+EDID_VERSION = 18
+EDID_REVISION = 19
+MAX_H_SIZE_CM = 21
+MAX_V_SIZE_CM = 22
+GAMMA = 23
+FEATURES = 24
+LO_RG_XY = 25
+LO_BW_XY = 26
+HI_R_X = 27
+HI_R_Y = 28
+HI_G_X = 29
+HI_G_Y = 30
+HI_B_X = 31
+HI_B_Y = 32
+HI_W_X = 33
+HI_W_Y = 34
+BLOCKS = ((54, 72), (72, 90), (90, 108), (108, 126))
+BLOCK_TYPE = 3
+BLOCK_CONTENTS = (5, 18)
+BLOCK_TYPE_SERIAL_ASCII = "\xff"
+BLOCK_TYPE_ASCII = "\xfe"
+BLOCK_TYPE_MONITOR_NAME = "\xfc"
+BLOCK_TYPE_COLOR_POINT = "\xfb"
+BLOCK_TYPE_COLOR_MANAGEMENT_DATA  = "\xf9"
+EXTENSION_FLAG = 126
+CHECKSUM = 127
+BLOCK_DI_EXT = "\x40"
+TRC = (81, 127)
+
 pnpidcache = {}
 
-def COMBINE_HI_8LO(hi, lo):
+def combine_hi_8lo(hi, lo):
 	return hi << 8 | lo
 
 
@@ -119,7 +155,7 @@ def parse_manufacturer_id(block):
 	The range is always ASCII charcode 64 to 95.
 	
 	"""
-	h = COMBINE_HI_8LO(ord(block[0]), ord(block[1]))
+	h = combine_hi_8lo(ord(block[0]), ord(block[1]))
 	manufacturer_id = []
 	for shift in (10, 5, 0):
 		manufacturer_id.append(chr(((h >> shift) & 0x1f) + ord('A') - 1))
@@ -170,55 +206,123 @@ def get_manufacturer_name(manufacturer_id):
 	return pnpidcache.get(manufacturer_id, manufacturer_id)
 
 
+def edid_get_bit(value, bit):
+	return (value & (1 << bit)) >> bit
+
+
+def edid_get_bits(value, begin, end):
+	mask = (1 << (end - begin + 1)) - 1
+	return (value >> begin) & mask
+
+
+def edid_decode_fraction(high, low):
+	result = 0.0
+	high = (high << 2) | low
+	for i in xrange(0, 10):
+		result += edid_get_bit(high, i) * math.pow(2, i - 10)
+	return result
+
+
 def parse_edid(edid):
 	""" Parse raw EDID data (binary string) and return dict. """
 	hash = md5(edid).hexdigest()
-	header = edid[0:8]
-	manufacturer_id = parse_manufacturer_id(edid[8:10])
+	header = edid[HEADER[0]:HEADER[1]]
+	manufacturer_id = parse_manufacturer_id(edid[MANUFACTURER_ID[0]:MANUFACTURER_ID[1]])
 	manufacturer = get_manufacturer_name(manufacturer_id)
 	
-	product_id = struct.unpack("<H", edid[10:12])[0]
-	serial_32 = struct.unpack("<I", edid[12:16])[0]
-	week_of_manufacture = ord(edid[16])
-	year_of_manufacture = ord(edid[17]) + 1990
-	edid_version = ord(edid[18])
-	edid_revision = ord(edid[19])
+	product_id = struct.unpack("<H", edid[PRODUCT_ID[0]:PRODUCT_ID[1]])[0]
+	serial_32 = struct.unpack("<I", edid[SERIAL_32[0]:SERIAL_32[1]])[0]
+	week_of_manufacture = ord(edid[WEEK_OF_MANUFACTURE])
+	year_of_manufacture = ord(edid[YEAR_OF_MANUFACTURE]) + 1990
+	edid_version = ord(edid[EDID_VERSION])
+	edid_revision = ord(edid[EDID_REVISION])
 	
-	max_h_size_cm = ord(edid[21])
-	max_v_size_cm = ord(edid[22])
-	gamma = ord(edid[23]) / 100.0 + 1
-	features = ord(edid[24])
+	max_h_size_cm = ord(edid[MAX_H_SIZE_CM])
+	max_v_size_cm = ord(edid[MAX_V_SIZE_CM])
+	if edid[GAMMA] != "\xff":
+		gamma = ord(edid[GAMMA]) / 100.0 + 1
+	features = ord(edid[FEATURES])
+	
+	red_x = edid_decode_fraction(ord(edid[HI_R_X]), 
+								 edid_get_bits(ord(edid[LO_RG_XY]), 6, 7))
+	red_y = edid_decode_fraction(ord(edid[HI_R_Y]), 
+								 edid_get_bits(ord(edid[LO_RG_XY]), 5, 4))
+	
+	green_x = edid_decode_fraction(ord(edid[HI_G_X]), 
+								   edid_get_bits(ord(edid[LO_RG_XY]), 2, 3))
+	green_y = edid_decode_fraction(ord(edid[HI_G_Y]), 
+								   edid_get_bits(ord(edid[LO_RG_XY]), 0, 1))
+
+	blue_x = edid_decode_fraction(ord(edid[HI_B_X]), 
+								  edid_get_bits(ord(edid[LO_BW_XY]), 6, 7))
+	blue_y = edid_decode_fraction(ord(edid[HI_B_Y]), 
+								  edid_get_bits(ord(edid[LO_BW_XY]), 4, 5))
+
+	white_x = edid_decode_fraction(ord(edid[HI_W_X]), 
+								   edid_get_bits(ord(edid[LO_BW_XY]), 2, 3))
+	white_y = edid_decode_fraction(ord(edid[HI_W_Y]), 
+								   edid_get_bits(ord(edid[LO_BW_XY]), 0, 1))
 	
 	result = locals()
 	
-	text_types = {"\xff": "serial_ascii",
-				  "\xfe": "ascii",
-				  "\xfc": "monitor_name"}
+	text_types = {BLOCK_TYPE_SERIAL_ASCII: "serial_ascii",
+				  BLOCK_TYPE_ASCII: "ascii",
+				  BLOCK_TYPE_MONITOR_NAME: "monitor_name"}
 	
-	# descriptor blocks
-	for block in (edid[54:72], edid[72:90], edid[90:108], edid[108:126]):
-		if block[:3] != "\0\0\0":
-			# ignore pixel clock data
+	# Parse descriptor blocks
+	for start, stop in BLOCKS:
+		block = edid[start:stop]
+		if block[:BLOCK_TYPE] != "\0\0\0":
+			# Ignore pixel clock data
 			continue
-		text_type = text_types.get(block[3])
+		text_type = text_types.get(block[BLOCK_TYPE])
 		if text_type:
 			# Make sure it's ASCII (charcode 0...127)
-			desc = block[5:18].strip().decode("ASCII", 
-											  "replace").encode("ASCII", 
-																"replace")
+			desc = block[BLOCK_CONTENTS[0]:BLOCK_CONTENTS[1]].strip().decode(
+				"ASCII", "replace").encode("ASCII", "replace")
 			# Filter out bogus strings
 			if desc.count("?") <= 4 < len(desc):
 				result[text_type] = desc.replace("?", "-")
-		elif block[3] == "\xfb":
-			# White point index structure
-			if block[5] == "\x01" and block[9] != "\xff":
-				block_type = "gamma"
-				i = 9
-			if block[10] == "\x02" and block[14] != "\xff":
-				block_type = "gamma"
-				i = 14
-			if block_type == "gamma":
-				result[block_type] = ord(block[i]) / 100.0 + 1
+		elif block[BLOCK_TYPE] == BLOCK_TYPE_COLOR_POINT:
+			for i in (5, 10):
+				# 2nd white point index in range 1...255
+				# 3rd white point index in range 2...255
+				# 0 = do not use
+				if ord(block[i]) > i / 5:
+					white_x = edid_decode_fraction(ord(edid[i + 2]), 
+												   edid_get_bits(ord(edid[i + 1]), 
+																 2, 3))
+					result["white_x_" + str(ord(block[i]))] = white_x
+					if not result.get("white_x"):
+						result["white_x"] = white_x
+					white_y = edid_decode_fraction(ord(edid[i + 3]), 
+												   edid_get_bits(ord(edid[i + 1]), 
+																 0, 1))
+					result["white_y_" + str(ord(block[i]))] = white_y
+					if not result.get("white_y"):
+						result["white_y"] = white_y
+					if block[i + 4] != "\xff":
+						gamma = ord(block[i + 4]) / 100.0 + 1
+						result["gamma_" + str(ord(block[i]))] = gamma
+						if not result.get("gamma"):
+							result["gamma"] = gamma
+		elif block[BLOCK_TYPE] == BLOCK_TYPE_COLOR_MANAGEMENT_DATA:
+			# TODO: Implement? How could it be used?
+			result["color_management_data"] = block[BLOCK_CONTENTS[0]:BLOCK_CONTENTS[1]]
+	
+	result["ext_flag"] = ord(edid[EXTENSION_FLAG])
+	result["checksum"] = ord(edid[CHECKSUM])
+	result["checksum_valid"] = sum(ord(char) for char in edid) % 256 == 0
+	
+	if len(edid) > 128 and result["ext_flag"] > 0:
+		# Parse extension blocks
+		block = edid[128:]
+		while block:
+			if block[0] == BLOCK_DI_EXT:
+				if block[TRC[0]] != "\0":
+					# TODO: Implement
+					pass
+			block = block[128:]
 	
 	return result
 
