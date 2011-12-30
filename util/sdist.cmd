45b4c5e402808933e843c1b29d8d847303e0a1ec
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2011-12-30 00:30:25 +0000

    Fix download URL for 0install feed
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@827 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- util/sdist.cmd
+++ util/sdist.cmd
@@ -13,5 +13,5 @@ for /F usebackq %%a in (`python -c "import os,time;print time.strftime('%%Y-%%m-
 )
 which 0launch.exe > nul && (
 	echo Updating 0install feed...
-	0launch http://0install.net/2006/interfaces/0publish --add-version=%version% --archive-url=http://dispcalgui.hoech.net/archive/%version%/dispcalGUI-%version%.tar.gz --archive-file=dist\dispcalGUI-%version%.tar.gz --set-main="dispcalGUI-%version%/dispcalGUI.pyw" --set-released="%released%" --set-stability=stable -x dist\0install\dispcalGUI.xml
+	0launch http://0install.net/2006/interfaces/0publish --add-version=%version% --archive-url="http://dispcalgui.hoech.net/download.php?version=%version%&suffix=.tar.gz" --archive-file=dist\dispcalGUI-%version%.tar.gz --set-main="dispcalGUI-%version%/dispcalGUI.pyw" --set-released="%released%" --set-stability=stable -x dist\0install\dispcalGUI.xml
 )
