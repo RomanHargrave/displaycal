ba4a921d02b632c22cf444e142ce98a9afc2faa5
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2012-06-07 02:37:29 +0000

    Reset default black point correction from automatic to off.
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@1033 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- dispcalGUI/config.py
+++ dispcalGUI/config.py
@@ -409,7 +409,7 @@ defaults = {
 	"calibration.black_luminance": 0.000001,
 	"calibration.black_output_offset": 1.0,
 	"calibration.black_point_correction": 0.0,
-	"calibration.black_point_correction.auto": 1,
+	"calibration.black_point_correction.auto": 0,
 	"calibration.black_point_correction_choice.show": 1,
 	"calibration.black_point_rate": 4.0,
 	"calibration.black_point_rate.enabled": 0,
