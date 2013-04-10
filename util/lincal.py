ee669c80c2adac20750f6cdb0acad81b978f5a9b
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2013-04-10 19:14:50 +0000

    Add number of steps as last argument
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@1313 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- util/lincal.py
+++ util/lincal.py
@@ -5,16 +5,19 @@ import math, os, sys
 from time import strftime
 
 if __name__ == '__main__':
-	if len(sys.argv) == 3:
+	if len(sys.argv) in (3, 4):
 		curve = []
 		start = float(sys.argv[1]) / 100
 		end = float(sys.argv[2]) / 100
+		steps = 256
+		if len(sys.argv) > 3:
+			steps = int(sys.argv[3])
 		if start == end:
 			print "ERROR: start == end"
 		else:
-			step = (end - start) / 255.0
+			step = (end - start) / (steps - 1)
 			i = start
-			for x in range(256):
+			for x in range(steps):
 				curve += [i]
 				i += step
 			if curve[-1] != end:
@@ -33,11 +36,12 @@ if __name__ == '__main__':
 			print 'RGB_I RGB_R RGB_G RGB_B'
 			print 'END_DATA_FORMAT'
 			print ''
-			print 'NUMBER_OF_SETS 256'
+			print 'NUMBER_OF_SETS %i' % steps
 			print 'BEGIN_DATA'
-			for i in range(256):
-				print " ".join([str(round(col, 5)).ljust(7, '0') for col in [(1.0 / 255) * i] + [curve[i]] * 3])
+			for i in range(steps):
+				print " ".join([str(round(col, 5)).ljust(7, '0') for col in [(1.0 / (steps - 1)) * i] + [curve[i]] * 3])
 			print 'END_DATA'
 	else:
-		print "Usage: %s start end" % os.path.basename(__file__)
+		print "Usage: %s start end steps" % os.path.basename(__file__)
 		print " start, end = integer between 0 and 100"
+		print " steps = number of steps (default 256)"
