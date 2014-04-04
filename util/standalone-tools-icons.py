5e20d5c068f850cb252f09e90e0733ea60605782
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2014-04-04 14:37:32 +0000

    Fix syntax error in previous commit and make sure to emit the correct filename in case of environment error as well as cleaning up the temporary directory
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@1910 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- util/standalone-tools-icons.py
+++ util/standalone-tools-icons.py
@@ -20,46 +20,62 @@ def installer(action="install"):
 	root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 	if action == "install":
 		tmpdir = mkdtemp()
-	for desktopfilename in glob(os.path.join(root, "misc", "%s-*.desktop" %
-														   appname)):
-		desktopbasename = os.path.basename(desktopfilename)
-		scriptname = re.sub("\.desktop$", "", desktopbasename)
+	tmpfilename = None
+	try:
+		for desktopfilename in glob(os.path.join(root, "misc", "%s-*.desktop" %
+															   appname)):
+			desktopbasename = os.path.basename(desktopfilename)
+			scriptname = re.sub("\.desktop$", "", desktopbasename)
+			if action == "install":
+				try:
+					for size in [16, 22, 24, 32, 48, 256]:
+						call(["xdg-icon-resource", action, "--noupdate",
+							  "--novendor", "--size", str(size),
+							  os.path.join([root, appname, "theme", "icons",
+											"%sx%s" % (size, size),
+											scriptname + ".png"])])
+				except EnvironmentError, exception:
+					exception.filename = "xdg-icon-resource"
+					raise exception
+				with open(desktopfilename) as desktopfile:
+					contents = desktopfile.read()
+				cmd = re.sub("^%s-" % appname, "run-", scriptname)
+				for pattern, repl in [("Exec=.+",
+									   "Exec=0launch --command=%s -- %s %%f" %
+									   (cmd, feeduri))]:
+					contents = re.sub(pattern, repl, contents)
+				tmpfilename = os.path.join(tmpdir, desktopbasename)
+				with open(tmpfilename, "w") as tmpfile:
+					tmpfile.write(contents)
+				try:
+					call(["xdg-desktop-menu", action, "--noupdate",
+						  "--novendor", tmpfilename])
+				except EnvironmentError, exception:
+					exception.filename = "xdg-desktop-menu"
+					raise exception
+			else:
+				try:
+					for size in [16, 22, 24, 32, 48, 256]:
+						call(["xdg-icon-resource", action, "--noupdate",
+							  "--size", str(size), scriptname])
+				except EnvironmentError, exception:
+					exception.filename = "xdg-icon-resource"
+					raise exception
+				try:
+					call(["xdg-desktop-menu", action, "--noupdate",
+						  desktopfilename])
+				except EnvironmentError, exception:
+					exception.filename = "xdg-desktop-menu"
+					raise exception
+	finally:
 		if action == "install":
-			with open(desktopfilename) as desktopfile:
-				contents = desktopfile.read()
-			cmd = re.sub("^%s-" % appname, "run-", scriptname)
-			for pattern, repl in [("Exec=.+",
-								   "Exec=0launch --command=%s -- %s %%f" %
-								   (cmd, feeduri))]:
-				contents = re.sub(pattern, repl, contents)
-			tmpfilename = os.path.join(tmpdir, desktopbasename)
-			with open(tmpfilename, "w") as tmpfile:
-				tmpfile.write(contents)
 			try:
-				for size in [16, 22, 24, 32, 48, 256]:
-					call(["xdg-icon-resource", action, "--noupdate",
-						  "--novendor", "--size", str(size),
-						  os.path.join([root, appname, "theme", "icons",
-										"%sx%s" % (size, size),
-										scriptname + ".png"])])
-				call(["xdg-desktop-menu", action, "--noupdate", "--novendor",
-						 tmpfilename])
-			except EnvironmentError, exception:
-				exception.filename = "xdg-desktop-menu"
-				raise exception
-			finally:
-				os.unlink(tmpfilename)
-		else:
-			for size in [16, 22, 24, 32, 48, 256]:
-				call(["xdg-icon-resource", action, "--noupdate",
-					  "--size", str(size), scriptname])])
-			call(["xdg-desktop-menu", action, "--noupdate", desktopfilename])
-	if action == "install":
-		try:
-			os.rmdir(tmpdir)
-		except Exception, exception:
-			import warnings
-			warnings.warn(exception, Warning)
+				if tmpfilename and os.path.isfile(tmpfilename):
+					os.unlink(tmpfilename)
+				os.rmdir(tmpdir)
+			except Exception, exception:
+				import warnings
+				warnings.warn(exception, Warning)
 	call(["xdg-icon-resource", "forceupdate"])
 	call(["xdg-desktop-menu", "forceupdate"])
 
