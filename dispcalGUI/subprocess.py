37b5c213d872ecaf075784557bc829da13763bef
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2014-04-20 15:46:39 +0000

    Improve subprocess.Popen: In case of an environment error, set its filename to the first item of args
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@2016 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- dispcalGUI/subprocess.py
+++ dispcalGUI/subprocess.py
@@ -13,3 +13,45 @@ if sys.platform == "win32":
 							  STARTF_USESTDHANDLES, STARTUPINFO, 
 							  STD_ERROR_HANDLE, STD_INPUT_HANDLE, 
 							  STD_OUTPUT_HANDLE, SW_HIDE, WAIT_OBJECT_0)
+
+
+_Popen = Popen
+_call = call
+_check_call = check_call
+
+
+class Popen(_Popen):
+
+	""" In case of an EnvironmentError when executing the child, set its
+		filename to the first item of args """
+
+	def __init__(self, *args, **kwargs):
+		try:
+			_Popen.__init__(self, *args, **kwargs)
+		except EnvironmentError, exception:
+			if not exception.filename:
+				if isinstance(args[0], basestring):
+					exception.filename = args[0].split()[0]
+				else:
+					exception.filename = args[0][0]
+			raise
+
+
+def call(*popenargs, **kwargs):
+    return Popen(*popenargs, **kwargs).wait()
+
+
+call.__doc__ = _call.__doc__
+
+
+def check_call(*popenargs, **kwargs):
+    retcode = call(*popenargs, **kwargs)
+    cmd = kwargs.get("args")
+    if cmd is None:
+        cmd = popenargs[0]
+    if retcode:
+        raise CalledProcessError(retcode, cmd)
+    return retcode
+
+
+check_call.__doc__ = _check_call.__doc__
