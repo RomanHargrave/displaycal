0d7d54754017103aec40b7213455ef7a1f7b121b
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2014-12-18 08:55:37 +0000

    Amend r2825 because it broke r2585 again
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@2854 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- dispcalGUI/wxLUTViewer.py
+++ dispcalGUI/wxLUTViewer.py
@@ -367,9 +367,14 @@ class LUTCanvas(plot.PlotCanvas):
 			else:
 				r_points = g_points = b_points = linear_points
 		
-		self.r_unique = len(set(round(y / axis_y * irange[-1]) for x, y in r_points))
-		self.g_unique = len(set(round(y / axis_y * irange[-1]) for x, y in g_points))
-		self.b_unique = len(set(round(y / axis_y * irange[-1]) for x, y in b_points))
+		# Note: We need to make sure each point is a float because it
+		# might be a decimal.Decimal, which can't be divided by floats!
+		self.r_unique = len(set(round(float(y) / axis_y * irange[-1])
+								for x, y in r_points))
+		self.g_unique = len(set(round(float(y) / axis_y * irange[-1])
+								for x, y in g_points))
+		self.b_unique = len(set(round(float(y) / axis_y * irange[-1])
+								for x, y in b_points))
 
 		legend = []
 		colour = None
