902ebd33f4ced67b479655b919afccf404daca50
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2015-01-08 10:27:58 +0000

    Amend previous commit: Fix typo & update irrespective of event type
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@2981 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- dispcalGUI/dispcalGUI.py
+++ dispcalGUI/dispcalGUI.py
@@ -9334,33 +9334,32 @@ class MainFrame(ReportFrame, BaseFrame):
 			setcfg("calibration.use_video_lut",
 				   getcfg("calibration.use_video_lut.backup"))
 			setcfg("calibration.use_video_lut.backup", None)
-		if not isinstance(event, CustomEvent):
-			update_delay_ctrls = False
-			if config.get_display_name() == "Resolve":
-				# Special case: Resolve. Needs a minimum display update delay of
-				# atleast 600 ms for repeatable measurements. This is a Resolve
-				# issue. There seem to be quite a few bugs that were introduced in
-				# Resolve via the version 10.1.x to 11.x transition.
-				if getcfg("measure.min_display_update_delay_ms", False) is None:
-					setcfg("measure.override_min_display_update_delay_ms.backup",
-						   getcfg("measure.override_min_display_update_delay_ms"))
-					setcfg("measure.min_display_update_delay_ms.backup",
-						   getcfg("measure.min_display_update_delay_ms"))
-				setcfg("measure.override_min_display_update_delay_ms", 1)
-				setcfg("measure.min_display_update_delay_ms", 600)
-				update_delay_ctrls = True
-			elif getcfg("calibration.use_video_lut.backup", False) is not None:
-				setcfg("measure.override_min_display_update_delay_ms",
-					   getcfg("measure.override_min_display_update_delay_ms.backup"))
-				setcfg("measure.min_display_update_delay_ms",
-					   getcfg("measure.min_display_update_delay_ms.backup"))
-				update_delay_ctrls = True
-			if update_delay_ctrls:
-				override = bool(getcfg("measure.override_min_display_update_delay_ms"))
-				getattr(self,
-						"override_min_display_update_delay_ms").SetValue(override)
-				self.update_display_delay_ctrl("min_display_update_delay_ms",
-											   override)
+		update_delay_ctrls = False
+		if config.get_display_name() == "Resolve":
+			# Special case: Resolve. Needs a minimum display update delay of
+			# atleast 600 ms for repeatable measurements. This is a Resolve
+			# issue. There seem to be quite a few bugs that were introduced in
+			# Resolve via the version 10.1.x to 11.x transition.
+			if getcfg("measure.min_display_update_delay_ms", False) is None:
+				setcfg("measure.override_min_display_update_delay_ms.backup",
+					   getcfg("measure.override_min_display_update_delay_ms"))
+				setcfg("measure.min_display_update_delay_ms.backup",
+					   getcfg("measure.min_display_update_delay_ms"))
+			setcfg("measure.override_min_display_update_delay_ms", 1)
+			setcfg("measure.min_display_update_delay_ms", 600)
+			update_delay_ctrls = True
+		elif getcfg("measure.min_display_update_delay_ms", False) is not None:
+			setcfg("measure.override_min_display_update_delay_ms",
+				   getcfg("measure.override_min_display_update_delay_ms.backup"))
+			setcfg("measure.min_display_update_delay_ms",
+				   getcfg("measure.min_display_update_delay_ms.backup"))
+			update_delay_ctrls = True
+		if update_delay_ctrls:
+			override = bool(getcfg("measure.override_min_display_update_delay_ms"))
+			getattr(self,
+					"override_min_display_update_delay_ms").SetValue(override)
+			self.update_display_delay_ctrl("min_display_update_delay_ms",
+										   override)
 		# Check if display is calibratable at all. Unset calibration update
 		# checkbox if this is not the case.
 		if config.is_uncalibratable_display():
