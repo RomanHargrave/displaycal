0df43b1b44fcbacfb448aa815b4703dd829e2a9a
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2015-04-02 17:28:48 +0000

    Amend previous commit: Log removed samples and warnings in the order they are encountered. Fix: Only remove samples if all components have <= 5% stimulus
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@3151 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- dispcalGUI/worker.py
+++ dispcalGUI/worker.py
@@ -6323,8 +6323,8 @@ usage: spotread [-options] [logfile]
 					device_labels.append(color_rep[0] + "_" + channel)
 				zeros = data.queryi(query)
 				errors = []
-				removed = []
-				for key in reversed(zeros.keys()):
+				remove = []
+				for key in zeros.keys():
 					sample = zeros[key]
 					device_values = [sample[label] for label in device_labels]
 					device_sum = 0
@@ -6332,22 +6332,23 @@ usage: spotread [-options] [logfile]
 						if value >= 5:
 							# Error on device values >= 5
 							errors.append(sample)
-							continue
+							self.log("Warning: Sample ID %i has %s = 0 and %s >= 5%%! (%s)" %
+									 (sample.SAMPLE_ID, color_rep[1], color_rep[0],
+									  " ".join(str(sample.queryv1(device_labels)).split())))
+							device_sum = 0
+							break
 						device_sum += value
 					if not device_sum:
-						# Skip device black
+						# Skip device black and device values >= 5
 						continue
+					# Queue sample for removal
+					remove.insert(0, sample)
+					self.log("Removed sample ID %i with %s = 0 and %s < 5%% (%s)" %
+							 (sample.SAMPLE_ID, color_rep[1], color_rep[0],
+							  " ".join(str(sample.queryv1(device_labels)).split())))
+				for sample in remove:
 					# Remove sample
 					data.pop(sample)
-					removed.insert(0, sample)
-				for sample in removed:
-					self.log("Removed sample ID %i with %s = 0 for %s < 5%% (%s)" %
-							 (sample.key, color_rep[1], color_rep[0],
-							  " ".join(str(sample.queryv1(device_labels)).split())))
-				for sample in errors:
-					self.log("Warning: Sample ID %i %s = 0 for %s >= 5%%! (%s)" %
-							 (sample.key, color_rep[1], color_rep[0],
-							  " ".join(str(sample.queryv1(device_labels)).split())))
 			ti3.write()
 		return cmd, args
 
