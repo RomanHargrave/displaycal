e6fa6e35ff10ed3f3b26809283cbac3811563161
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2015-04-30 02:47:37 +0000

    Fix annoying findall bug in distutils which can't deal with symlinks pointing to non-existent files
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@3228 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- dispcalGUI/setup.py
+++ dispcalGUI/setup.py
@@ -41,6 +41,25 @@ import sys
 from time import strftime
 from types import StringType
 
+
+# Borrowed from setuptools
+def findall(dir=os.curdir):
+	"""Find all files under 'dir' and return the list of full filenames
+	(relative to 'dir').
+	"""
+	all_files = []
+	for base, dirs, files in os.walk(dir, followlinks=True):
+		if base == os.curdir or base.startswith(os.curdir + os.sep):
+			base = base[2:]
+		if base:
+			files = [os.path.join(base, f) for f in files]
+		all_files.extend(filter(os.path.isfile, files))
+	return all_files
+
+import distutils.filelist
+distutils.filelist.findall = findall    # Fix findall bug in distutils
+
+
 from defaultpaths import autostart, autostart_home
 from meta import (author, author_ascii, description, longdesc, domain, name, 
 				  py_maxversion, py_minversion, version, version_tuple, 
