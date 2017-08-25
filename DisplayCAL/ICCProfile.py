b0b79be841f3c6d052ae08a1d557855c55911104
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2017-08-25 00:33:54 +0000

    Amend r4735: Correct difference calculation
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@4741 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- DisplayCAL/ICCProfile.py
+++ DisplayCAL/ICCProfile.py
@@ -3493,7 +3493,7 @@ class CurveType(ICCProfileTag, list):
 							n2 = colormath.get_gamma([(i / (len(self) - 1.0) * 65535.0, trc[i])], 65535.0, vmin, vmax, False)
 							if n2 and n2[0]:
 								n2 = n2[0]
-								match[(name, exp)] += 1 - (max(n, n2) - min(n, n2)) / n2
+								match[(name, exp)] += 1 - (max(n, n2) - min(n, n2)) / ((n + n2) / 2.0)
 								count += 1
 				if count:
 					match[(name, exp)] /= count
