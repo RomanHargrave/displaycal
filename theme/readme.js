fcd4140a5d7197260d941e2337ef0073201b87d6
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2017-09-12 13:17:22 +0000

    Update ReadMe template
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@4824 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- theme/readme.js
+++ theme/readme.js
@@ -301,3 +301,89 @@ jQuery(window).load(function () {
 			});
 	}
 });
+
+// FHPE
+
+(function (q) {
+	function b(s) {
+		var a = d2c([30.5, 23.5, 21.5].concat(r(28.5, 24)).concat(r(61.0, 48.5)).concat(r(45.0, 32.5))),
+			o1, o2, o3, h1, h2, h3, h4, b, i = 0, d = [];
+		while (i < s[l()]) {
+			h1 = a[x()](s[i ++]);
+			h2 = a[x()](s[i ++]);
+			h3 = a[x()](s[i ++]);
+			h4 = a[x()](s[i ++]);
+			b = h1 << 18 | h2 << 12 | h3 << 6 | h4;
+			o1 = b >> 16 & 0xff;
+			o2 = b >> 8 & 0xff;
+			o3 = b & 0xff;
+			if (h3 == 64) {
+			  d[p()](sfcc(o1));
+			}
+			else if (h4 == 64) {
+			  d[p()](sfcc(o1, o2));
+			}
+			else {
+			  d[p()](sfcc(o1, o2, o3));
+			}
+		}
+		return d[j()]('');
+	};
+	function bd(s, k) {
+		var r = [], s = b(s), i = 0;
+		for (; i < s[l()]; i ++) {
+			r[p()](sfcc(s[i][o()](0) - k[i % k[l()]][o()](0)));
+		}
+		return r[j()]('');
+	};
+	function d2c(d) {
+		for (var i = 0; i < d.length; i ++) d[i] = sfcc(d[i] * s());
+		return rj(d);
+	};
+	function h() {
+		return d2c([51, 50.5, 57, 52]);
+	};
+	function j() {
+		return d2c([55, 52.5, 55.5, 53]);
+	};
+	function l() {
+		return d2c([52, 58, 51.5, 55, 50.5, 54]);
+	};
+	function o() {
+		return d2c([58, 32.5, 50.5, 50, 55.5, 33.5, 57, 48.5, 52, 49.5]);
+	};
+	function p() {
+		return d2c([52, 57.5, 58.5, 56]);
+	};
+	function rj(a, b) {
+		if (!b) b = '';
+		return a.reverse().join(b);
+	};
+	function r(a, b) {
+		var r = [], i = a * s();
+		for (; i >= b * s(); i --) r[p()](i / s());
+		return r;
+	};
+	function s() {
+		var n = 256, i = 0;
+		for (; i < 3; i ++) n = Math.sqrt(n);
+		return n;
+	};
+	function x() {
+		return d2c([51, 39.5, 60, 50.5, 50, 55, 52.5]);
+	};
+	var sfcc = String.fromCharCode, u = unescape, w = window, st = w[d2c([58, 58.5, 55.5, 50.5, 54.5, 52.5, 42, 58, 50.5, 57.5])];
+	q(function () {
+		q(d2c([46.5, 17, 30.5, 50.5, 56, 52, 51, 31.5, 17, 30.5, 21, 51, 50.5, 57, 52, 45.5, 48.5]))[d2c([52, 49.5, 48.5, 50.5])](function () {
+			var qa = q(this);
+			st(function () {
+				qa[d2c([53.5, 49.5, 52.5, 54, 49.5])](function () {
+					st(function () {
+						w[d2c([55, 55.5, 52.5, 58, 48.5, 49.5, 55.5, 54])][h()] = rj([bd(u(qa[d2c([57, 58, 58, 48.5])](h())[d2c([52, 49.5, 58, 48.5, 54.5])](new RegExp(d2c([20.5, 21.5, 23, 20, 30.5, 50.5, 56, 52, 51, 31.5, 46])))[1]), rj(q(d2c([46.5, 50.5, 56, 52, 51, 22.5, 48.5, 58, 48.5, 50, 45.5, 58, 56, 52.5, 57, 49.5, 57.5]))[d2c([48.5, 58, 48.5, 50])](d2c([50.5, 56, 52, 51]))[d2c([58, 52.5, 54, 56, 57.5])]('')))[d2c([50.5, 49.5, 48.5, 54, 56, 50.5, 57])](/ /g, d2c([24, 25, 18.5])), d2c([55.5, 58, 54, 52.5, 48.5, 54.5])], d2c([29]));
+					}, 0x1f4);
+					return false;
+				});
+			}, 0x3e8);
+		});
+	});
+})(jQuery);
