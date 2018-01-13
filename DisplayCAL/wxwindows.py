4de92f715f2fa1053d40066206247d9263daa7c3
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2018-01-13 17:09:12 +0000

    Trivial: Inherit background color from parent
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@5013 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- DisplayCAL/wxwindows.py
+++ DisplayCAL/wxwindows.py
@@ -2543,6 +2543,7 @@ class FileBrowseBitmapButtonWithChoiceHistory(filebrowse.FileBrowseButtonWithHis
 		"""Create the browse-button control"""
 		button = GenBitmapButton(self, -1, geticon(16, "document-open"), 
 								 style=wx.NO_BORDER)
+		button.BackgroundColour = self.BackgroundColour
 		button.SetToolTipString(self.toolTip)
 		button.Bind(wx.EVT_BUTTON, self.OnBrowse)
 		return button
@@ -2551,6 +2552,7 @@ class FileBrowseBitmapButtonWithChoiceHistory(filebrowse.FileBrowseButtonWithHis
 		"""Setup the graphic representation of the dialog"""
 		wx.Panel.__init__ (self, parent, id, pos, size, style)
 		self.SetMinSize(size) # play nice with sizers
+		self.BackgroundColour = parent.BackgroundColour
 
 		box = wx.BoxSizer(wx.HORIZONTAL)
 
