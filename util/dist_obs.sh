c6a8ea20b916b486d96ad5ba33c2abee15053b6e
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2018-04-07 20:00:29 +0000

    Update OBS dist script
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@5222 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- util/dist_obs.sh
+++ util/dist_obs.sh
@@ -7,21 +7,28 @@ appname=`python -c "from DisplayCAL import meta;print meta.name"`
 version=`python -c "from DisplayCAL import meta;print meta.version"`
 
 # OpenSUSE build service
-pushd ../obs/home:fhoech/$appname
-osc update
-osc service localrun
-# Remove previous version
-for filename in *.tar.gz ; do
-	echo "$filename" | grep "^$appname-" > /dev/null && (
-		echo "$filename" | grep "^$appname-$version" > /dev/null || (
-			osc remove $filename
+obs_home_repo=home:fhoech/$appname
+obs_m_cm_repo=multimedia:color_management/$appname
+for obs_repo in "$obs_home_repo" "$obs_m_cm_repo"; do 
+	pushd "../obs/$obs_repo"
+	osc update
+	osc service localrun
+	# Remove previous version
+	for filename in *.tar.gz ; do
+		echo "$filename" | grep "^$appname-" > /dev/null && (
+			echo "$filename" | grep "^$appname-$version" > /dev/null || (
+				osc remove $filename
+			)
 		)
-	)
+	done
+	# Remove *.tar.gz.1 file (OSC bug when there is a debian target)
+	for filename in *.tar.gz.1 ; do
+		osc remove --force $filename
+	done
+	if [ "$obs_repo" == "$obs_m_cm_repo" ] && [ ! -e "$appname-$version.tar.gz" ]; then
+		cp "../../$obs_home_repo/$appname-$version.tar.gz" .
+	fi
+	osc addremove
+	osc ci -m "Update to version $version"
+	popd
 done
-# Remove *.tar.gz.1 file (OSC bug when there is a debian target)
-for filename in *.tar.gz.1 ; do
-	osc remove --force $filename
-done
-osc addremove
-osc ci -m "Update to version $version"
-popd
