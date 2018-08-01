a833c0a153b9496c45a303fe28aaa63154c59bbb
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2018-08-01 01:38:27 +0000

    Optimize Cube3DIterator
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@5437 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- DisplayCAL/cubeiterator.py
+++ DisplayCAL/cubeiterator.py
@@ -3,64 +3,29 @@
 
 class Cube3DIterator(object):
 
-	def __init__(self, size=65, start=0, end=None,
-				 start_column0=0, start_column1=0, start_column2=0,
-				 end_column0=None, end_column1=None, end_column2=None):
+	def __init__(self, size=65, start=0, end=None):
 		self._size = size
 		self._start = start
-		self._end = end or size ** 3
-		self._start_column0 = start_column0
-		self._start_column1 = start_column1
-		self._start_column2 = start_column2
-		self._end_column0 = end_column0 or size
-		self._end_column1 = end_column1 or size
-		self._end_column2 = end_column2 or size
-		self._len = self._end - start
-		self._column0 = start_column0
-		self._column1 = start_column1
-		self._column2 = start_column2
-		self._current = start
+		self._len = (end or size ** 3) - start
+		self._next = 0
 
-	# def _get_point(self, i, start=0, start_column0=0, start_column1=0, start_column2=0):
-		# if i < 0:
-			# i = self._len + i
-		# elif i > self._len:
-			# return (self._len, self._end_column0 - 1, self._end_column1 - 1, self._end_column2 - 1)
-		# n = start
-		# found = False
-		# for column0 in xrange(start_column0, self._end_column0):
-			# for column1 in xrange(start_column1, self._end_column1):
-				# for column2 in xrange(start_column2, self._end_column2):
-					# if n == i:
-						# found = True
-						# break
-					# n += 1
-				# if found:
-					# break
-			# if found:
-				# break
-		# return i, column0, column1, column2
-
-	def __getitem__(self, i):
-		if not self._len:
-			raise IndexError("index %i out of range" % i)
+	def get(self, i, default=None):
 		if i < 0:
 			i = self._len + i
-		if i + self._start != self._current:
-			# Optimize for consecutive iteration speed
-			if i + self._start < self._current:
-				self._column0 = self._start_column0
-				self._column1 = self._start_column1
-				self._column2 = self._start_column2
-				self._current = self._start
-				start = 0
-			else:
-				start = self._current - self._start
-			i -= start
-			while i:
-				self.next()
-				i -= 1
-		return self._column0, self._column1, self._column2
+		if i < 0 or i > self._len - 1:
+			return default
+		return self[i]
+
+	def __getitem__(self, i):
+		oi = i
+		if i < 0:
+			i = self._len + i
+		if i < 0 or i > self._len - 1:
+			raise IndexError("index %i out of range" % oi)
+		i += self._start
+		return (i // self._size // self._size,
+				i // self._size % self._size,
+				i % self._size)
 	
 	def __getslice__(self, i, j):
 		if i < 0:
@@ -69,37 +34,19 @@ class Cube3DIterator(object):
 			j = self._len + j
 		elif j > self._len:
 			j = self._len
-		start_column0, start_column1, start_column2 = self[i]
-		end_column0, end_column1, end_column2 = self[j - 1]
-		return self.__class__(self._size, i, j,
-							  start_column0, start_column1, start_column2,
-							  end_column0 + 1, end_column1 + 1, end_column2 + 1)
+		return self.__class__(self._size, self._start + i, self._start + j)
 
 	def __len__(self):
 		return self._len
 
 	def __repr__(self):
-		return (self.__class__.__name__ +
-				"(size=%i, start=%i, end=%i,"
-				" start_column0=%i, start_column1=%i, start_column2=%i,"
-				" end_column0=%i, end_column1=%i, end_column2=%i)" %
-				(self._size, self._start, self._end,
-				 self._column0, self._column1, self._column2,
-				 self._end_column0, self._end_column1, self._end_column2))
+		return (self.__class__.__name__ + "(size=%i, start=%i, end=%i)" %
+				(self._size, self._start, self._start + self._len))
 
 	def next(self):
-		if self._current == self._end - 1:
+		if self._next == self._len:
 			raise StopIteration
 		else:
-			result = self._column0, self._column1, self._column2
-			if self._column2 < self._size - 1:
-				self._column2 += 1
-			else:
-				self._column2 = 0
-				if self._column1 < self._size - 1:
-					self._column1 += 1
-				else:
-					self._column1 = 0
-					self._column0 += 1
-			self._current += 1
+			result = self[self._next]
+			self._next += 1
 			return result
