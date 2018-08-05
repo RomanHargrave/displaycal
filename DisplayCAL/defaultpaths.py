32faeba40917737d43913ec0429f5036a3fe250d
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2018-08-05 22:24:38 +0000

    Implement full XDG user dirs compatibility
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@5457 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- DisplayCAL/defaultpaths.py
+++ DisplayCAL/defaultpaths.py
@@ -3,8 +3,13 @@
 from __future__ import with_statement
 import os
 import sys
+if sys.platform not in ("darwin", "win32"):
+	# Linux
+	import codecs
+	import locale
+	import gettext
 
-if sys.platform == "win32":
+elif sys.platform == "win32":
 	try:
 		from win32com.shell.shell import SHGetSpecialFolderPath
 		from win32com.shell.shellcon import (CSIDL_APPDATA, 
@@ -30,7 +35,7 @@ if sys.platform == "win32":
 														  create)
 			return buffer.value
 
-from util_os import expanduseru, expandvarsu, getenvu
+from util_os import expanduseru, expandvarsu, getenvu, waccess
 
 
 def get_known_folder_path(folderid, user=True):
@@ -38,12 +43,14 @@ def get_known_folder_path(folderid, user=True):
 	Get known folder path.
 	
 	Uses GetKnownFolderPath API on Windows Vista and later, and XDG user dirs
-	on Linux (if configured).
+	on Linux.
 	
 	Falls back to ~/<folderid> in all other cases.
 	
 	folderid can be "Desktop", "Downloads", "Documents", "Music", "Pictures",
 	"Public", "Templates", or "Videos".
+
+	user   Return user folder instead of common (Windows) or default (Linux)
 	
 	"""
 	folder_path = os.path.join(expanduseru("~"), folderid)
@@ -61,18 +68,16 @@ def get_known_folder_path(folderid, user=True):
 		# Linux
 		folderid = {"Downloads": folderid[:-1],
 					"Public": folderid + "share"}.get(folderid, folderid).upper()
-		user_dirs = os.path.join(xdg_config_home, "user-dirs.dirs")
-		if os.path.isfile(user_dirs):
-			try:
-				with open(user_dirs, "r") as f:
-					for line in f:
-						if line.startswith("XDG_%s_DIR" % folderid):
-							folder_path = expandvarsu(line.split("=")[-1].strip().strip('"'))
-							break
-			except EnvironmentError, exception:
-				from log import safe_print
-				safe_print("Warning: Could not get known folder %r:" %
-						   folderid, exception)
+		user_dir = XDG.default_dirs.get(folderid)
+		if user:
+			user_dir = XDG.user_dirs.get(folderid, user_dir)
+		if user_dir:
+			folder_path = os.path.join(expandvarsu("$HOME"), user_dir)
+		if (folderid != "DESKTOP" and (not user_dir or
+									   (not os.path.isdir(folder_path) and
+									    not XDG.enabled))) or not waccess(folder_path,
+																		  os.W_OK):
+			folder_path = expandvarsu("$HOME")
 	return folder_path
 
 
@@ -199,3 +204,173 @@ else:
 	iccprofiles_display_home = [os.path.join(dir_, "devices", "display") 
 								for dir_ in iccprofiles_home]
 	del dir_
+
+
+if sys.platform not in ("darwin", "win32"):
+	# Linux
+
+	class XDG:
+
+		GETTEXT_PACKAGE = "xdg-user-dirs"
+		LOCALE_DIR = os.path.join(sys.prefix, "share", "locale")
+
+		enabled = True
+		filename_encoding = "UTF-8"
+		default_dirs = {}
+		user_dirs = {}
+
+		@staticmethod
+		def init_locale():
+			locale.setlocale(locale.LC_ALL, "")
+
+			locale_dir = XDG.LOCALE_DIR
+
+			if not os.path.isdir(locale_dir):
+				for path in xdg_data_dirs:
+					path = os.path.join(path, "locale")
+					if os.path.isdir(path):
+						locale_dir = path
+						break
+
+			try:
+				XDG.translation = gettext.translation(XDG.GETTEXT_PACKAGE,
+													  locale_dir,
+													  codeset="UTF-8")
+			except IOError, exception:
+				from log import safe_print
+				safe_print("XDG:", exception)
+				XDG.translation = gettext.NullTranslations()
+				return False
+			return True
+
+		@staticmethod
+		def is_true(s):
+			return s == "1" or s.startswith("True") or s.startswith("true")
+
+		@staticmethod
+		def get_config_files(filename):
+			paths = []
+
+			for xdg_config_dir in [xdg_config_home] + xdg_config_dirs:
+				path = os.path.join(xdg_config_dir, filename)
+				if os.path.isfile(path):
+					paths.append(path)
+
+			return paths
+
+		@staticmethod
+		def load_config(path):
+			def fn(key, value):
+				if key == "enabled":
+					XDG.enabled = XDG.is_true(value)
+				elif key == "filename_encoding":
+					value = value.upper()
+					if value == "LOCALE":
+						XDG.filename_encoding = locale.nl_langinfo(locale.CODESET)
+					else:
+						XDG.filename_encoding = value
+
+			return XDG.process_config_file(path, fn)
+
+		@staticmethod
+		def load_all_configs():
+			for path in reversed(XDG.get_config_files("user-dirs.conf")):
+				XDG.load_config(path)
+
+		@staticmethod
+		def load_default_dirs():
+			paths = XDG.get_config_files("user-dirs.defaults")
+			if not paths:
+				from log import safe_print
+				safe_print("XDG: No default user directories")
+				return False
+
+			def fn(name, path):
+				XDG.default_dirs[name] = XDG.localize_path_name(path)
+
+			return XDG.process_config_file(paths[0], fn)
+
+		@staticmethod
+		def load_user_dirs():
+			path = os.path.join(xdg_config_home, "user-dirs.dirs")
+			if not path or not os.path.isfile(path):
+				return False
+
+			def fn(key, value):
+				if (key.startswith("XDG_") and key.endswith("_DIR") and
+					value.startswith('"') and value.endswith('"')):
+					name = key[4:-4]
+					if not name:
+						return
+					value = value.strip('"')
+					if value.startswith('$HOME'):
+						value = value[5:]
+						if value.startswith("/"):
+							value = value[1:]
+						elif value:
+							# Not ending after $HOME, nor followed by slash.
+							# Ignore
+							return
+					elif not value.startswith("/"):
+						return
+					XDG.user_dirs[name] = XDG.shell_unescape(value).decode("UTF-8",
+																		   "ignore")
+
+			return XDG.process_config_file(path, fn)
+
+		@staticmethod
+		def localize_path_name(path):
+			elements = path.split(os.path.sep)
+
+			for i, element in enumerate(elements):
+				elements[i] = XDG.translation.ugettext(element)
+
+			return os.path.join(*elements)
+
+		@staticmethod
+		def shell_unescape(s):
+			a = []
+			for i, c in enumerate(s):
+				if c == "\\" and len(s) > i + 1:
+					continue
+				a.append(c)
+			return "".join(a)
+
+		@staticmethod
+		def config_file_parser(f):
+			for line in f:
+				line = line.strip()
+				if line.startswith("#") or not "=" in line:
+					continue
+				yield tuple(s.strip() for s in line.split("="))
+
+		@staticmethod
+		def process_config_file(path, fn):
+			try:
+				with open(path, "r") as f:
+					for key, value in XDG.config_file_parser(f):
+						fn(key, value)
+			except EnvironmentError, exception:
+				from log import safe_print
+				safe_print("XDG: Couldn't read '%s':" % path, exception)
+				return False
+			return True
+
+		@staticmethod
+		def init():
+			XDG.init_locale()
+
+			XDG.load_all_configs()
+			try:
+				codecs.lookup(XDG.filename_encoding)
+			except LookupError:
+				from log import safe_print
+				safe_print("XDG: Can't convert from UTF-8 to",
+						   XDG.filename_encoding)
+				return False
+
+			XDG.load_default_dirs()
+			XDG.load_user_dirs()
+
+
+	XDG.init()
