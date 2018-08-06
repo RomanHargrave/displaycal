e556a85ee9e64c0a1acd992ca49b4703e801993e
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2018-08-06 04:21:14 +0000

    Refactor XDG
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@5459 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- DisplayCAL/defaultpaths.py
+++ DisplayCAL/defaultpaths.py
@@ -9,6 +9,10 @@ if sys.platform not in ("darwin", "win32"):
 	import locale
 	import gettext
 
+	locale.setlocale(locale.LC_ALL, "")
+
+	LOCALEDIR = os.path.join(sys.prefix, "share", "locale")
+
 elif sys.platform == "win32":
 	try:
 		from win32com.shell.shell import SHGetSpecialFolderPath
@@ -69,16 +73,16 @@ def get_known_folder_path(folderid, user=True):
 		user_dir = folderid
 		folderid = {"Downloads": folderid[:-1],
 					"Public": folderid + "share"}.get(folderid, folderid).upper()
-		if folderid != "DESKTOP" or XDG.enabled:
-			user_dir = XDG.default_dirs.get(folderid)
+		if folderid != "DESKTOP" or XDG.UserDirs.enabled:
+			user_dir = XDG.UserDirs.default_dirs.get(folderid)
 		if user:
-			user_dir = XDG.user_dirs.get(folderid, user_dir)
+			user_dir = XDG.UserDirs.user_dirs.get(folderid, user_dir)
 		if user_dir:
 			folder_path = os.path.join(expandvarsu("$HOME"), user_dir)
-		if (folderid != "DESKTOP" and (not user_dir or
-									   (not os.path.isdir(folder_path) and
-									    not XDG.enabled))) or not waccess(folder_path,
-																		  os.W_OK):
+		if ((folderid != "DESKTOP" and (not user_dir or
+										(not os.path.isdir(folder_path) and
+										 not XDG.UserDirs.enabled))) or
+			not waccess(folder_path, os.W_OK)):
 			folder_path = expandvarsu("$HOME")
 	return folder_path
 
@@ -158,90 +162,47 @@ elif sys.platform == "darwin":
 	programs = os.path.join(os.path.sep, "Applications")
 	commonprograms = []
 else:
-	cache = xdg_cache_home = getenvu("XDG_CACHE_HOME",
-									 expandvarsu("$HOME/.cache"))
-	xdg_config_home = getenvu("XDG_CONFIG_HOME", expandvarsu("$HOME/.config"))
-	xdg_config_dir_default = "/etc/xdg"
-	xdg_config_dirs = [os.path.normpath(pth) for pth in 
-					   getenvu("XDG_CONFIG_DIRS", 
-							   xdg_config_dir_default).split(os.pathsep)]
-	if not xdg_config_dir_default in xdg_config_dirs:
-		xdg_config_dirs.append(xdg_config_dir_default)
-	xdg_data_home_default = expandvarsu("$HOME/.local/share")
-	library_home = appdata = xdg_data_home = getenvu("XDG_DATA_HOME", xdg_data_home_default)
-	xdg_data_dirs_default = "/usr/local/share:/usr/share:/var/lib"
-	xdg_data_dirs = [os.path.normpath(pth) for pth in 
-					 getenvu("XDG_DATA_DIRS", 
-							 xdg_data_dirs_default).split(os.pathsep)]
-	for dir_ in xdg_data_dirs_default.split(os.pathsep):
-		if not dir_ in xdg_data_dirs:
-			xdg_data_dirs.append(dir_)
-	commonappdata = xdg_data_dirs
-	library = commonappdata[0]
-	autostart = None
-	for dir_ in xdg_config_dirs:
-		if os.path.exists(dir_):
-			autostart = os.path.join(dir_, "autostart")
-			break
-	if not autostart:
-		autostart = os.path.join(xdg_config_dir_default, "autostart")
-	autostart_home = os.path.join(xdg_config_home, "autostart")
-	iccprofiles = []
-	for dir_ in xdg_data_dirs:
-		if os.path.exists(dir_):
-			iccprofiles.append(os.path.join(dir_, "color", "icc"))
-	iccprofiles.append("/var/lib/color")
-	iccprofiles_home = [os.path.join(xdg_data_home, "color", "icc"), 
-						os.path.join(xdg_data_home, "icc"), 
-						expandvarsu("$HOME/.color/icc")]
-	programs = os.path.join(xdg_data_home, "applications")
-	commonprograms = [os.path.join(dir_, "applications")
-					  for dir_ in xdg_data_dirs]
-if sys.platform in ("darwin", "win32"):
-	iccprofiles_display = iccprofiles
-	iccprofiles_display_home = iccprofiles_home
-else:
-	iccprofiles_display = [os.path.join(dir_, "devices", "display") 
-						   for dir_ in iccprofiles]
-	iccprofiles_display_home = [os.path.join(dir_, "devices", "display") 
-								for dir_ in iccprofiles_home]
-	del dir_
-
-
-if sys.platform not in ("darwin", "win32"):
 	# Linux
 
 	class XDG:
 
-		GETTEXT_PACKAGE = "xdg-user-dirs"
-		LOCALE_DIR = os.path.join(sys.prefix, "share", "locale")
-
-		enabled = True
-		filename_encoding = "UTF-8"
-		default_dirs = {}
-		user_dirs = {}
+		cache_home = getenvu("XDG_CACHE_HOME", expandvarsu("$HOME/.cache"))
+		config_home = getenvu("XDG_CONFIG_HOME", expandvarsu("$HOME/.config"))
+		config_dir_default = "/etc/xdg"
+		config_dirs = map(os.path.normpath,
+						  getenvu("XDG_CONFIG_DIRS", 
+								  config_dir_default).split(os.pathsep))
+		if not config_dir_default in config_dirs:
+			config_dirs.append(config_dir_default)
+		data_home_default = expandvarsu("$HOME/.local/share")
+		data_home = getenvu("XDG_DATA_HOME", data_home_default)
+		data_dirs_default = "/usr/local/share:/usr/share:/var/lib"
+		data_dirs = map(os.path.normpath,
+						getenvu("XDG_DATA_DIRS",
+								data_dirs_default).split(os.pathsep))
+		data_dirs.extend(filter(lambda data_dir, data_dirs=data_dirs:
+								not data_dir in data_dirs,
+								data_dirs_default.split(os.pathsep)))
 
 		@staticmethod
-		def init_locale():
-			locale.setlocale(locale.LC_ALL, "")
-
-			locale_dir = XDG.LOCALE_DIR
+		def set_translation(obj):
+			locale_dir = LOCALEDIR
 
 			if not os.path.isdir(locale_dir):
-				for path in xdg_data_dirs:
+				for path in XDG.data_dirs:
 					path = os.path.join(path, "locale")
 					if os.path.isdir(path):
 						locale_dir = path
 						break
 
 			try:
-				XDG.translation = gettext.translation(XDG.GETTEXT_PACKAGE,
+				obj.translation = gettext.translation(obj.GETTEXT_PACKAGE,
 													  locale_dir,
 													  codeset="UTF-8")
 			except IOError, exception:
 				from log import safe_print
 				safe_print("XDG:", exception)
-				XDG.translation = gettext.NullTranslations()
+				obj.translation = gettext.NullTranslations()
 				return False
 			return True
 
@@ -253,81 +214,12 @@ if sys.platform not in ("darwin", "win32"):
 		def get_config_files(filename):
 			paths = []
 
-			for xdg_config_dir in [xdg_config_home] + xdg_config_dirs:
+			for xdg_config_dir in [XDG.config_home] + XDG.config_dirs:
 				path = os.path.join(xdg_config_dir, filename)
 				if os.path.isfile(path):
 					paths.append(path)
 
 			return paths
-
-		@staticmethod
-		def load_config(path):
-			def fn(key, value):
-				if key == "enabled":
-					XDG.enabled = XDG.is_true(value)
-				elif key == "filename_encoding":
-					value = value.upper()
-					if value == "LOCALE":
-						XDG.filename_encoding = locale.nl_langinfo(locale.CODESET)
-					else:
-						XDG.filename_encoding = value
-
-			return XDG.process_config_file(path, fn)
-
-		@staticmethod
-		def load_all_configs():
-			for path in reversed(XDG.get_config_files("user-dirs.conf")):
-				XDG.load_config(path)
-
-		@staticmethod
-		def load_default_dirs():
-			paths = XDG.get_config_files("user-dirs.defaults")
-			if not paths:
-				from log import safe_print
-				safe_print("XDG: No default user directories")
-				return False
-
-			def fn(name, path):
-				XDG.default_dirs[name] = XDG.localize_path_name(path)
-
-			return XDG.process_config_file(paths[0], fn)
-
-		@staticmethod
-		def load_user_dirs():
-			path = os.path.join(xdg_config_home, "user-dirs.dirs")
-			if not path or not os.path.isfile(path):
-				return False
-
-			def fn(key, value):
-				if (key.startswith("XDG_") and key.endswith("_DIR") and
-					value.startswith('"') and value.endswith('"')):
-					name = key[4:-4]
-					if not name:
-						return
-					value = value.strip('"')
-					if value.startswith('$HOME'):
-						value = value[5:]
-						if value.startswith("/"):
-							value = value[1:]
-						elif value:
-							# Not ending after $HOME, nor followed by slash.
-							# Ignore
-							return
-					elif not value.startswith("/"):
-						return
-					XDG.user_dirs[name] = XDG.shell_unescape(value).decode("UTF-8",
-																		   "ignore")
-
-			return XDG.process_config_file(path, fn)
-
-		@staticmethod
-		def localize_path_name(path):
-			elements = path.split(os.path.sep)
-
-			for i, element in enumerate(elements):
-				elements[i] = XDG.translation.ugettext(element)
-
-			return os.path.join(*elements)
 
 		@staticmethod
 		def shell_unescape(s):
@@ -358,21 +250,140 @@ if sys.platform not in ("darwin", "win32"):
 				return False
 			return True
 
-		@staticmethod
-		def init():
-			XDG.init_locale()
+		class _UserDirs(object):
 
-			XDG.load_all_configs()
-			try:
-				codecs.lookup(XDG.filename_encoding)
-			except LookupError:
-				from log import safe_print
-				safe_print("XDG: Can't convert from UTF-8 to",
-						   XDG.filename_encoding)
-				return False
+			GETTEXT_PACKAGE = "xdg-user-dirs"
 
-			XDG.load_default_dirs()
-			XDG.load_user_dirs()
+			enabled = True
+			filename_encoding = "UTF-8"
+			default_dirs = {}
+			user_dirs = {}
 
+			_initialized = False
 
-	XDG.init()
+			def __getattribute__(self, name):
+				if (name != "init" and not
+					object.__getattribute__(self, "_initialized")):
+					object.__getattribute__(self, "init")()
+				return object.__getattribute__(self, name)
+
+			def load_config(self, path):
+				def fn(key, value):
+					if key == "enabled":
+						self.enabled = XDG.is_true(value)
+					elif key == "filename_encoding":
+						value = value.upper()
+						if value == "LOCALE":
+							self.filename_encoding = locale.nl_langinfo(locale.CODESET)
+						else:
+							self.filename_encoding = value
+
+				return XDG.process_config_file(path, fn)
+
+			def load_all_configs(self):
+				for path in reversed(XDG.get_config_files("user-dirs.conf")):
+					self.load_config(path)
+
+			def load_default_dirs(self):
+				paths = XDG.get_config_files("user-dirs.defaults")
+				if not paths:
+					from log import safe_print
+					safe_print("XDG.UserDirs: No default user directories")
+					return False
+
+				def fn(name, path):
+					self.default_dirs[name] = self.localize_path_name(path)
+
+				return XDG.process_config_file(paths[0], fn)
+
+			def load_user_dirs(self):
+				path = os.path.join(XDG.config_home, "user-dirs.dirs")
+				if not path or not os.path.isfile(path):
+					return False
+
+				def fn(key, value):
+					if (key.startswith("XDG_") and key.endswith("_DIR") and
+						value.startswith('"') and value.endswith('"')):
+						name = key[4:-4]
+						if not name:
+							return
+						value = value.strip('"')
+						if value.startswith('$HOME'):
+							value = value[5:]
+							if value.startswith("/"):
+								value = value[1:]
+							elif value:
+								# Not ending after $HOME, nor followed by slash.
+								# Ignore
+								return
+						elif not value.startswith("/"):
+							return
+						self.user_dirs[name] = XDG.shell_unescape(value).decode("UTF-8",
+																			   "ignore")
+
+				return XDG.process_config_file(path, fn)
+
+			def localize_path_name(self, path):
+				elements = path.split(os.path.sep)
+
+				for i, element in enumerate(elements):
+					elements[i] = self.translation.ugettext(element)
+
+				return os.path.join(*elements)
+
+			def init(self):
+				self._initialized = True
+
+				XDG.set_translation(self)
+
+				self.load_all_configs()
+				try:
+					codecs.lookup(self.filename_encoding)
+				except LookupError:
+					from log import safe_print
+					safe_print("XDG.UserDirs: Can't convert from UTF-8 to",
+							   self.filename_encoding)
+					return False
+
+				self.load_default_dirs()
+				self.load_user_dirs()
+
+		UserDirs = _UserDirs()
+
+	for name in dir(XDG):
+		if isinstance(name, (basestring, list)):
+			locals()["xdg_" + name] = getattr(XDG, name)
+	del name
+
+	cache = XDG.cache_home
+	library_home = appdata = XDG.data_home
+	commonappdata = XDG.data_dirs
+	library = commonappdata[0]
+	autostart = None
+	for dir_ in XDG.config_dirs:
+		if os.path.isdir(dir_):
+			autostart = os.path.join(dir_, "autostart")
+			break
+	if not autostart:
+		autostart = os.path.join(XDG.config_dir_default, "autostart")
+	autostart_home = os.path.join(XDG.config_home, "autostart")
+	iccprofiles = []
+	for dir_ in XDG.data_dirs:
+		if os.path.isdir(dir_):
+			iccprofiles.append(os.path.join(dir_, "color", "icc"))
+	iccprofiles.append("/var/lib/color")
+	iccprofiles_home = [os.path.join(XDG.data_home, "color", "icc"), 
+						os.path.join(XDG.data_home, "icc"), 
+						expandvarsu("$HOME/.color/icc")]
+	programs = os.path.join(XDG.data_home, "applications")
+	commonprograms = [os.path.join(dir_, "applications")
+					  for dir_ in XDG.data_dirs]
+if sys.platform in ("darwin", "win32"):
+	iccprofiles_display = iccprofiles
+	iccprofiles_display_home = iccprofiles_home
+else:
+	iccprofiles_display = [os.path.join(dir_, "devices", "display") 
+						   for dir_ in iccprofiles]
+	iccprofiles_display_home = [os.path.join(dir_, "devices", "display") 
+								for dir_ in iccprofiles_home]
+	del dir_
