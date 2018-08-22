a11cfae6da4717472f2bd0119c2a90d098ed7a3a
fhoech <fhoech@6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd>
2018-08-22 14:22:50 +0000

    Add MacBook to identified models with integrated display
    git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@5491 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

--- DisplayCAL/DisplayCAL.py
+++ DisplayCAL/DisplayCAL.py
@@ -10342,7 +10342,7 @@ class MainFrame(ReportFrame, BaseFrame):
 		if sys.platform == "darwin":
 			# In case of internal screen, get 'nice' description
 			model_id = util_mac.get_model_id()
-			if model_id and re.match("iBook|iMac|PowerBook", model_id,
+			if model_id and re.match("iBook|iMac|MacBook|PowerBook", model_id,
 									 flags=re.I):
 				attrs = util_mac.get_machine_attributes(model_id) or {}
 				description = description.replace(model_id,
